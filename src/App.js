d1d2611b23d958493b1cb72807349ab518f99c19
laurent <laurent@langkawi.localdomain>
2018-03-16 11:46:23 +0100

    get users from JSON API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: '',
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			// .then(response => {
+			// 	return response.json();
+			// })
+			// .then(users => {
+			// 	this.setState({ robots : users})
+			// })
+			.then(response => response.json())
+			.then(users => this.setState({ robots : users}));
+
+		
 	}
 
 	onSearchChange = (event) => {
@@ -21,14 +34,21 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		})
-		
-		return (
+
+		if(this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return (
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<CardList robots={filteredRobots}/>
 			</div>
 		);
+
+		}
+		
+		
 	}
 }
 
